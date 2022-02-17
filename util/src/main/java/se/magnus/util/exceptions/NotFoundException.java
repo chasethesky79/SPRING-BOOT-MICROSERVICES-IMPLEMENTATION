d6d1e797dea86531c1c5a5410697a66019114ad0
Bharath Seshadri <bharathseshadri@kukubara7.local>
2022-02-17 08:02:32 -0600

    Added exception related classes

---
+++ util/src/main/java/se/magnus/util/exceptions/NotFoundException.java
@@ -0,0 +1,13 @@
+package se.magnus.util.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotFoundException extends RuntimeException {
+  private String message;
+  private Throwable cause;
+}
